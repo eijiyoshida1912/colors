9c4d0fe90a377b87341800e64df60ae2c23b243d
Eiji Yoshida <eijiyoshida1912@gmail.com>
2021-02-16 23:41:36 +0900

    ボタンを押すと色が変わるところまで実装

--- app/javascript/packs/questions_vue.js
+++ app/javascript/packs/questions_vue.js
@@ -2,44 +2,59 @@ import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 import Question from '../question.vue';
 
+const colors = {
+  black: "黒",
+  white: "白",
+  red: "赤",
+  orange: "オレンジ",
+  yellow: "黄",
+  green: "緑",
+  blue: "青",
+  purple: "紫",
+  brown: "茶",
+  gray: "灰色",
+  pink: "ピンク"
+}
+
+Vue.filter('buttonName', (value) => {
+  value = parseInt(value) + 1;
+  return "answer[q" + value + "]";
+})
+
+Vue.filter('buttonId', (value, color) => {
+  value = parseInt(value) + 1;
+  return "answer_q" + value + "_" + color;
+})
+
 Vue.component('question', {
   template: `
-      <div class="questions_list">
- {{ radio_value }}
+      <div class="questions_list" :class="radio_value">
         <h2 class="questions_list__q">{{ question }}</h2>
-
-        <span class="questions_list__button black">
-          <label>黒</label>
-          <input type="radio" value="black" name="answer[q1]" id="answer_q1_black" v-model="radio_value" />
-        </span>
-
+        <ul class="button_list">
+          <li v-for="(item, index) in colors" class="button_list__button" :class="index">
+            <label :for="buttonId(number, index)">{{ item }}</label>
+            <input type="radio" :value="index" :name="number | buttonName" :id="buttonId(number, index)" v-model="radio_value" />
+          </li>
+        </ul>
       </div>
   `,
   props: ['question', 'number'],
-  data: {
-    radio_value: ''
+  data: function() {
+    return {
+      radio_value: '',
+      colors: colors
+    }
+  },
+  methods: {
+    buttonId: function(number, color) {
+      number = parseInt(number) + 1;
+      return "answer_q" + number + "_" + color;
+    }
   }
 })
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#questions',
-    data: {
-      colors: {
-        black: "黒",
-        white: "白",
-        red: "赤",
-        orange: "オレンジ",
-        yellow: "黄",
-        green: "緑",
-        blue: "青",
-        purple: "紫",
-        brown: "茶",
-        gray: "灰色",
-        pink: "ピンク"
-      },
-      radio_value: ''
-    },
   })
 })
