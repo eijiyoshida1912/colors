c0560349a07248d1d760454d83cabcac38c9c819
Eiji Yoshida <eijiyoshida1912@gmail.com>
2021-02-23 23:11:58 +0900

    結果の表示完了

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,68 +4,16 @@ class ResultsController < ApplicationController
     @questions = QUESTIONS
     @colors = COLORS
 
-    # 問に対して黒と答えた人の数
-    @answerblack = []
-    @answerwhite = []
-    @answerred = []
-    @answerorange = []
-    @answeryellow = []
-    @answergreen = []
-    @answerblue = []
-    @answerpurple = []
-    @answerbrown = []
-    @answergray = []
-    @answerpink = []
-
-
-    1.upto(103) do |i|
-      @answerblack.push(Answer.where("q#{i}": "black").count)
-      @answerwhite.push(Answer.where("q#{i}": "white").count)
-      @answerred.push(Answer.where("q#{i}": "red").count)
-      @answerorange.push(Answer.where("q#{i}": "orange").count)
-      @answeryellow.push(Answer.where("q#{i}": "yellow").count)
-      @answergreen.push(Answer.where("q#{i}": "green").count)
-      @answerblue.push(Answer.where("q#{i}": "blue").count)
-      @answerpurple.push(Answer.where("q#{i}": "purple").count)
-      @answerbrown.push(Answer.where("q#{i}": "brown").count)
-      @answergray.push(Answer.where("q#{i}": "gray").count)
-      @answerpink.push(Answer.where("q#{i}": "pink").count)
+    # 問に対してそれぞれの色を答えた人の数。
+    @colors.each do |color_k, color_v|
+      # ハッシュのインスタンス変数を生成
+      instance_variable_set("@answer#{color_k}", {})
+      # 回答数をユーザーの数で割ってパーセントを出す
+      @questions.each_with_index do |q, i|
+        instance_variable_get("@answer#{color_k}")["#{q}"] = (((Answer.where("q#{i+1}": "#{color_k}").count).to_f / @answers.count) * 100).round(0)
+      end
+      # 回答数が0の物は削除して、多い順番に並べ直す
+      instance_variable_set("@answer#{color_k}", instance_variable_get("@answer#{color_k}").delete_if{|k, v| v == 0}.sort_by{|_, v| -v})
     end
-
-
-    
-    @answerq1black = Answer.where(q1: "black").count
-    @answerq1white = Answer.where(q1: "white").count
-    @answerq1red = Answer.where(q1: "red").count
-    @answerq1orange = Answer.where(q1: "orange").count
-    @answerq1yellow = Answer.where(q1: "yellow").count
-    @answerq1green = Answer.where(q1: "green").count
-    @answerq1blue = Answer.where(q1: "blue").count
-    @answerq1purple = Answer.where(q1: "purple").count
-    @answerq1brown = Answer.where(q1: "brown").count
-    @answerq1gray = Answer.where(q1: "gray").count
-    @answerq1pink = Answer.where(q1: "pink").count
-
-
-    
-  end
-
-  def show
-    render plain: "show"
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
