be1318bbf74621dd1e20e7388223a1a3d24764d3
Eiji Yoshida <eijiyoshida1912@gmail.com>
2021-03-18 19:44:32 +0900

    ポスグレにした

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_15_140134) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "q1", null: false
     t.string "q2", null: false
